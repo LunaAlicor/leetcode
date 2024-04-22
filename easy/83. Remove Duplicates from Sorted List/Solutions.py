f3918eb3a28a36f9de66fe259abf2953de713082
LunaAlicor <84154835+LunaAlicor@users.noreply.github.com>
2024-04-22 06:32:12 +0300

    83. Remove Duplicates from Sorted List
    83. Remove Duplicates from Sorted List

---
+++ easy/83. Remove Duplicates from Sorted List/Solutions.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def deleteDuplicates(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        if not head or not head.next:
+            return head
+        
+        current = head
+        
+        while current and current.next:
+            if current.val == current.next.val:
+                current.next = current.next.next
+            else:
+                current = current.next
+        
+        return head
